2c0cfdbc9295410c2d9b4d97c697584be21066c0
MariaDiezEsteve <diezesteve@outlook.es>
2023-01-17 14:27:56 +0100

    incluido la explicación de javascript para que salga en javascript_bucle

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 console.log("Hello World")
 
+// Iteración de 1 al 21: impreso en el console.log
 
-function bucle() {
 
-    for (let i = 0; i < 21; i++) {
-        console.log(i)     
-        
+function bucle1() {
+
+    for (let i = 1; i < 21; i++) {
+        console.log(i)   
     }
-
 } 
 
-bucle()
+bucle1()
+
+// Iteración dándole valores para que nos imprima el rango con los dos valores
+
+// function bucle2(start, finish) {
+
+//     for (let i = start; i < finish; i++) {
+//         // console.log(i)   
+        
+//         document.write(i + "<br>");
+//     }
+// } 
+
+// bucle2(10,40)
+
+
+
